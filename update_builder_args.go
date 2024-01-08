a44f250946da30c732977eaa05735bfaf2babe47
Vinh Vo <vohuynhvinh@gmail.com>
2024-01-08 19:46:24 +0700

    Update unit test for cases the update builder with argument

---
+++ update_builder_args.go
@@ -0,0 +1,129 @@
+package fluentsql
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Sql Get Query statement and Arguments
+func (ub *UpdateBuilder) Sql() (string, []any, interface{}) {
+	return ub.StringArgs()
+}
+
+func (ub *UpdateBuilder) StringArgs() (string, []any, error) {
+	var queryParts []string
+	var sql string
+	var args []any
+
+	sql, args = ub.updateStatement.StringArgs(args)
+	queryParts = append(queryParts, sql)
+
+	sql, args = ub.setStatement.StringArgs(args)
+	queryParts = append(queryParts, sql)
+
+	sql, args = ub.whereStatement.StringArgs(args)
+	if sql != "" {
+		queryParts = append(queryParts, sql)
+	}
+
+	sql, args = ub.orderByStatement.StringArgs(args)
+	if sql != "" {
+		queryParts = append(queryParts, sql)
+	}
+
+	sql, args = ub.limitStatement.StringArgs(args)
+	if sql != "" {
+		queryParts = append(queryParts, sql)
+	}
+
+	sql = strings.Join(queryParts, " ")
+
+	return sql, args, nil
+}
+
+func (u *Update) StringArgs(args []any) (string, []any) {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("UPDATE %s", u.Table))
+
+	if u.Alias != "" {
+		sb.WriteString(" " + u.Alias)
+	}
+
+	return sb.String(), args
+}
+
+func (s *UpdateSet) StringArgs(args []any) (string, []any) {
+	var setColumns []string
+
+	for _, item := range s.Items {
+		var sql string
+
+		sql, args = item.StringArgs(args)
+
+		setColumns = append(setColumns, sql)
+	}
+
+	return fmt.Sprintf("SET %s", strings.Join(setColumns, ", ")), args
+}
+
+func (s *UpdateItem) StringArgs(args []any) (string, []any) {
+	// SET (field1, field2,...) = (int, string, ValueField...)
+	// SET (field1, field2,...) = (SELECT * FROM table_name)
+	if fieldStringSlice, ok := s.Field.([]string); ok {
+		fieldStr := joinSlice(fieldStringSlice, ",")
+
+		if valueQueryBuilder, ok := s.Value.(*QueryBuilder); ok { // Value type is QueryBuilder.
+			var _sql string
+			_sql, args, _ = valueQueryBuilder.StringArgs(args)
+
+			return fmt.Sprintf("(%s) = (%s)", fieldStr, _sql), args
+		}
+
+		if fieldAnySlice, ok := s.Value.([]any); ok { // Value type is slice
+			var values []string
+			for _, fieldAny := range fieldAnySlice {
+				if valueField, ok := fieldAny.(ValueField); ok { // Value type is string.
+					values = append(values, valueField.String())
+				} else if valueString, ok := fieldAny.(string); ok { // Value type is string.
+					args = append(args, valueString)
+					valueStr := p(args)
+
+					values = append(values, valueStr)
+				} else { // Value type is int or float.
+					args = append(args, valueString)
+					valueStr := p(args)
+
+					values = append(values, valueStr)
+				}
+			}
+
+			valueStr := strings.Join(values, ", ")
+
+			return fmt.Sprintf("(%s) = (%v)", fieldStr, valueStr), args
+		}
+
+		return "", args
+	}
+
+	if valueQueryBuilder, ok := s.Value.(*QueryBuilder); ok { // Value type is QueryBuilder.
+		var _sql string
+		_sql, args, _ = valueQueryBuilder.StringArgs(args)
+
+		return fmt.Sprintf("%s = (%s)", s.Field, _sql), args
+	}
+
+	if valueField, ok := s.Value.(ValueField); ok { // Value type is string.
+		return fmt.Sprintf("%s = %s", s.Field, valueField), args
+	}
+
+	if valueString, ok := s.Value.(string); ok { // Value type is string.
+		args = append(args, valueString)
+		valueStr := p(args)
+
+		return fmt.Sprintf("%s = %s", s.Field, valueStr), args
+	}
+	args = append(args, s.Value)
+	valueStr := p(args)
+
+	return fmt.Sprintf("%s = %s", s.Field, valueStr), args
+}
--- update_builder_test.go
+++ update_builder_test.go
@@ -45,3 +45,52 @@ func TestUpdateBasic(t *testing.T) {
 		}
 	}
 }
+
+// TestUpdateArgs
+func TestUpdateArgs(t *testing.T) {
+	testCases := map[string]*UpdateBuilder{
+		"UPDATE Customers SET ContactName = $1, City = $2 WHERE CustomerID = $3": UpdateInstance().
+			Update("Customers").
+			Set("ContactName", "Alfred Schmidt").
+			Set("City", "Frankfurt").
+			Where("CustomerID", Eq, 1),
+		"UPDATE Customers SET ContactName = $1, City = Location WHERE CustomerID = $2": UpdateInstance().
+			Update("Customers").
+			Set("ContactName", "Alfred Schmidt").
+			Set("City", ValueField("Location")).
+			Where("CustomerID", Eq, 5),
+		"UPDATE CUSTOMERS SET ADDRESS = $1, SALARY = $2 WHERE NAME = $3": UpdateInstance().
+			Update("CUSTOMERS").
+			Set("ADDRESS", "Pune").
+			Set("SALARY", 1000.00).
+			Where("NAME", Eq, "Ramesh"),
+		"UPDATE dependents SET last_name = (SELECT last_name FROM employees WHERE employee_id = dependents.employee_id)": UpdateInstance().
+			Update("dependents").
+			Set("last_name", QueryInstance().
+				Select("last_name").
+				From("employees").
+				Where("employee_id", Eq, ValueField("dependents.employee_id")),
+			),
+		"UPDATE summary s SET (sum_x, sum_y, avg_x, avg_y) = (SELECT sum(x), sum(y), avg(x), avg(y) FROM data d WHERE d.group_id = s.group_id)": UpdateInstance().
+			Update("summary", "s").
+			Set([]string{"sum_x", "sum_y", "avg_x", "avg_y"}, QueryInstance().
+				Select("sum(x)", "sum(y)", "avg(x)", "avg(y)").
+				From("data", "d").
+				Where("d.group_id", Eq, ValueField("s.group_id")),
+			),
+		"UPDATE summary SET (sum_x, sum_y, avg_x) = ($1, $2, $3)": UpdateInstance().
+			Update("summary").
+			Set([]string{"sum_x", "sum_y", "avg_x"}, []any{1, "One", 34.5}),
+	}
+
+	for expected, query := range testCases {
+		var sql string
+		var args []any
+
+		sql, args, _ = query.Sql()
+
+		if sql != expected {
+			t.Fatalf(`Query %s != %s (%v)`, sql, expected, args)
+		}
+	}
+}
